8c03a8313f8c5b3eae38335d79da0d27e11548e4
Sly1029 <tihor29@gmail.com>
2017-03-07 13:04:45 -0500

    3/7

--- ATCompSci/src/sets/DupRunner.java
+++ ATCompSci/src/sets/DupRunner.java
@@ -7,7 +7,7 @@ public class DupRunner
 	public static void main( String args[] )
 	{
 		String a = "abcdefghijklmnde";
-		System.out.println("Original List: "+ a);
+		System.out.println("Original List: "+ a);a
 		System.out.println("Uniques: "+UniquesDupes.getUniques(a));
 		System.out.println("Duplicates: "+ UniquesDupes.getDupes(a));
 	}
