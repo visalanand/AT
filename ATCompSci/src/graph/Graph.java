e035b89504eed6db418bb8d46d9d49b6383c508c
Rohit J <alttihor29@gmail.com>
2017-06-08 09:39:54 -0400

    6/8

--- ATCompSci/src/graph/Graph.java
+++ ATCompSci/src/graph/Graph.java
@@ -1,32 +1,105 @@
+package graph;
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -  
 //Lab  -
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Graph
 {
-	private TreeMap<String, String> map;
+	
 	private boolean yahOrNay;
-
-	public Graph(String line)
+	Map<Character, String> ar  = new TreeMap<Character, String>();
+	public Graph(Map <Character, String> _map)
 	{
+		ar = _map;
 	}
 
-	public boolean contains(String letter)
+	
+	public void readFile(String temp) throws FileNotFoundException
 	{
-	   return true;
+		
+		Map<Character , String> map = new TreeMap<Character, String>();
+		String token;
+		char a , b;
+		StringTokenizer str ;
+		
+			str = new StringTokenizer (temp);
+			while ( str.hasMoreTokens()){
+				try{
+				
+			token = str.nextToken();
+			a = token.charAt(0);
+			b = token.charAt(1);
+			if (map.get(a) != null){
+				String tempRemove = map.get(a);
+				map.remove(a);
+				map.put(a, tempRemove+b);
+			}
+			else if ( map.get(a)== null){
+				map.put(a, b+"");
+			}
+			if (map.get(b) != null){
+				String tempRemove = map.get(b);
+				map.remove(b);
+				map.put(b, tempRemove+a);
+			}
+			else if ( map.get(b)== null){
+				map.put(b, a+"");
+			}
+			
+			
+			}	catch(Exception E){
+				break;
+			}
+			}
+			
+			
+		ar = map;
+		
+	}
+		
+	
+	
+	
+	public boolean connected (char a , char b,String been){
+		if (ar.get(a) == null)
+			return false;
+		
+		
+		if(ar.get(a).contains(""+b)){
+			return true;
+		}
+		
+		else {
+		for ( int x = 0; x<ar.get(a).length();x++){
+			if(!been.contains(""+ar.get(a).charAt(x)))
+			if (connected(ar.get(a).charAt(x),b,been+ar.get(a).charAt(x))){
+				return true;
+			}
+		}
+		return false;
+		}
+	
+		
+		
 	}
 
-	public void check(String first, String second, String placesUsed)
-	{
-	}
-
+	
 	public String toString()
 	{
 		return "";
 	}
+	
+	
+	
+	
 }
--- ATCompSci/src/graph/GraphRunner.java
+++ ATCompSci/src/graph/GraphRunner.java
@@ -1,4 +1,6 @@
+package graph;
 //© A+ Computer Science  -  www.apluscompsci.com
+
 //Name -
 //Date -
 //Class -
@@ -6,19 +8,32 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
 import static java.lang.System.*;
 
-public class GraphRunner
-{
-	public static void main( String[] args ) throws IOException
-	{
-		Scanner file = new Scanner(new File("graph.dat"));
-		int howManyTimes = file.nextInt();
-		file.nextLine();
-		for(int x=0; x<howManyTimes; x++)
-		{
-			//add code here
+public class GraphRunner {
+	public static void main(String[] args) throws IOException {
+		Graph graph = new Graph (null);
+		
+		Scanner file = new Scanner(new File("H:\\graph1.dat"));
+		int x = Integer.parseInt(file.nextLine())	;
+		String n;
+		while (file.hasNextLine()){
+			graph.readFile(file.nextLine());
+			if (file.hasNextLine())
+			 n= file.nextLine();
+			else {
+				break;
+			}
+			System.out.println(n.charAt(0)+" is Connected to "+n.charAt(1)+" === "+graph.connected(n.charAt(0), n.charAt(1),""));
 		}
+		
+		
+		
+		
 	}
 }
